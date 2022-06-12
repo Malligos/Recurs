f49e61070a14fed11bf4d91bbefd8c35ce4c0665
Malligos <butssergei7778@gmail.com>
2022-06-13 00:23:48 +0300

    Recurse

---
+++ js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let student = {
+    js: [{
+        name: 'Jone',
+        progress: 100
+    }, {
+        name: 'Ivan',
+        progress: 60
+    }],
+
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress:20
+        }, {
+            name: 'Ann',
+            progress: 18
+        }],
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }]
+    }
+};
+
---
+++ js_1/script.js
@@ -0,0 +1,41 @@
+//let x = 5; alert( x++ ); 5
+
+//[] + false - null + true; NaN
+
+// let y = 1;
+// let x = y = 2;
+// alert( x );    2
+
+//console.log ([] + 1 + 2); 12
+
+//alert ('1'[0]); 1
+
+//console.log(2 && 1 && null && 0 && undefined); null
+
+
+//console.log (!!( 1 && 2 ) === ( 1 && 2)); false
+
+
+//alert (null || 2 && 3 || 4);  3
+
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
+// console.log (a == b); false
+
+//alert ( +"Infinity"); Infinity
+
+
+
+//console.log ('Ежик' > 'Яблоко'); false
+
+
+// console.log (0 || "" || 2 || undefined || true || false); 2
+
+// const box = document.querySelectorAll('.helo');
+
+// // for (let i = 0; i < box.length; i++) {
+// //     box[i].style.listStyle = 'none';
+// // }
+
+// box.forEach(item => {
+//     item.style.listStyle = 'none';
